6689d4318d51bbeee1e6107db2ea83fb195968d0
Qurratulain844 <qurratulainsyed.22@gmail.com>
2026-03-24 20:54:41 +0530

    fix build.sh remove makemigrations

--- backend/build.sh
+++ backend/build.sh
@@ -2,11 +2,5 @@
 
 pip install -r requirements.txt
 
-echo "Running collectstatic..."
 python manage.py collectstatic --noinput
-
-echo "Running migrations..."
-python manage.py makemigrations
 python manage.py migrate
-
-echo "Build complete"
